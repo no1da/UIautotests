bdf73c653790ef20cc3af3874fb9098ccec85914
no1da <99120974+no1da@users.noreply.github.com>
2024-11-23 18:43:59 +0400

    U5Cookies

---
+++ src/test/java/utils/CookieManager.java
@@ -0,0 +1,59 @@
+package utils;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+/**
+ * Утилита для управления куками в браузере с использованием WebDriver.
+ * Позволяет сохранять и загружать куки в формате JSON.
+ */
+public class CookieManager {
+    private static final Gson gson = new Gson();
+    private WebDriver driver;
+    /**
+     * Сохраняет куки из текущей сессии WebDriver в указанный файл.
+     *
+     * @param driver  экземпляр WebDriver, из которого будут извлекаться куки.
+     * @param filePath путь к файлу, куда будут сохранены куки в формате JSON.
+     */
+    public static void saveCookies(WebDriver driver, String filePath) {
+        Set<Cookie> cookies = driver.manage().getCookies();
+        Map<String, String> cookieMap = new HashMap<>();
+        System.out.println(cookies);
+        for (Cookie cookie : cookies) {
+            cookieMap.put(cookie.getName(), cookie.getValue());
+        }
+        try (Writer writer = new FileWriter(filePath)) {
+            gson.toJson(cookieMap, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Загружает куки из указанного файла и добавляет их в текущую сессию WebDriver.
+     *
+     * @param driver  экземпляр WebDriver, куда будут добавлены загруженные куки.
+     * @param filePath путь к файлу, из которого будут загружены куки в формате JSON.
+     */
+    public static void loadCookies(WebDriver driver, String filePath) {
+        try (Reader reader = new FileReader(filePath)) {
+            Type cookieMapType = new TypeToken<Map<String, String>>() {}.getType();
+            Map<String, String> cookieMap = gson.fromJson(reader, cookieMapType);
+
+            for (Map.Entry<String, String> entry : cookieMap.entrySet()) {
+                String name = entry.getKey();
+                String value = entry.getValue();
+                driver.manage().addCookie(new Cookie(name, value));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
