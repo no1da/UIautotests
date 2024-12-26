bf18033a263fac7816522d649b3a77d8d1e4d2f6
no1da <99120974+no1da@users.noreply.github.com>
2024-12-26 07:30:08 +0400

    ref

--- src/test/java/tests/steps/WayPageSteps.java
+++ src/test/java/tests/steps/WayPageSteps.java
@@ -3,58 +3,55 @@ package tests.steps;
 import io.cucumber.java.ru.Дано;
 import io.cucumber.java.ru.Тогда;
 import org.assertj.core.api.Assertions;
-import org.openqa.selenium.WebDriver;
 import pages.WayPage;
 import tests.hooks.TestHooks;
 import utils.Config;
 
-public class WayPageSteps {
-    private WebDriver driver;
-    private WayPage wayPage;
+public class WayPageSteps extends WayPage {
+
     private Config config;
 
     public WayPageSteps() {
-        this.driver = TestHooks.getDriver();
+        super(TestHooks.getDriver());
         this.config = new Config();
-        this.wayPage = new WayPage(driver);
     }
 
     @Дано("я открываю страницу Way2Automation")
-    public void iOenThePage() {
+    public void openThePage() {
             driver.get(config.getProperty("app.url"));
     }
 
     @Тогда("я должен увидеть заголовок")
-    public void iShouldSeeTheHeader() {
-        Assertions.assertThat(wayPage.findHeader())
+    public void shouldSeeTheHeader() {
+        Assertions.assertThat(findHeader())
                 .as("Header should be displayed")
                 .isTrue();
     }
 
     @Тогда("я должен увидеть футер")
-    public void iShouldSeeTheFooter() {
-        Assertions.assertThat(wayPage.findFooter())
+    public void shouldSeeTheFooter() {
+        Assertions.assertThat(findFooter())
                 .as("Footer should be displayed")
                 .isTrue();
     }
 
     @Тогда("я должен увидеть кнопку регистрации")
-    public void iShouldSeeTheRegistrationButton() {
-        Assertions.assertThat(wayPage.findButtonRegistration())
+    public void shouldSeeTheRegistrationButton() {
+        Assertions.assertThat(findButtonRegistration())
                 .as("Registration button should be displayed")
                 .isTrue();
     }
 
     @Тогда("я должен увидеть список курсов")
-    public void iShouldSeeTheCoursesList() {
-        Assertions.assertThat(wayPage.findListCourses())
+    public void shouldSeeTheCoursesList() {
+        Assertions.assertThat(findListCourses())
                 .as("Courses list should be displayed")
                 .isTrue();
     }
 
     @Тогда("я должен увидеть блок навигации")
-    public void iShouldSeeTheNavigationBlock() {
-        Assertions.assertThat(wayPage.findNavigationBlock())
+    public void shouldSeeTheNavigationBlock() {
+        Assertions.assertThat(findNavigationBlock())
                 .as("Navigation block should be displayed")
                 .isTrue();
     }
